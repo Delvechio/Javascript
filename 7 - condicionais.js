853ff3f97f9d1a16091cc65350920a7118d03fa2
Delvechio Duarte da Silva Júnior <delvechioduarte2502@gmail.com>
2021-09-17 11:56:33 -0300

    Condicionais 'or' 'and'.

--- 7 - condicionais.js
+++ 7 - condicionais.js
@@ -7,21 +7,35 @@ const listaDeDestinos = new Array(
 
 const idadeDoComprador = 15;
 const estaAcompanhada = true;
+const temPassagemComprada = true;
 console.log("Destinos possíveis:");
 console.log(listaDeDestinos);
 
-if (idadeDoComprador >= 18) {
-    console.log("Comprador maior de idade!")
-    listaDeDestinos.splice(1, 1); // deletando um item na lista
+//if (idadeDoComprador >= 18) {
+//      console.log("Comprador maior de idade!")
+//      listaDeDestinos.splice(1, 1); // deletando um item na lista
+//} else {
+//    // a pessoa é menor de idade
+//    if (estaAcompanhada) {
+//        console.log("Comprador maior de idade!")
+//        listaDeDestinos.splice(1, 1);
+//    } else {
+//        console.log("Não é maior de idade não posso vender");
+//    }
+
+//} // nas condiçoes vc pode usar o else if ou o else e dentro do else fazer uma outra condição
+
+if (idadeDoComprador >= 18 || estaAcompanhada == true) {
+    console.log("Comprador maior idade");
+    listaDeDestinos.slice(2,1);
 } else {
-    // a pessoa é menor de idade
-    if (estaAcompanhada) {
-        console.log("Comprador maior de idade!")
-        listaDeDestinos.splice(1, 1);
-    } else {
-        console.log("Não é maior de idade não posso vender");
-    }
+    console.log("Não é maior de idade e não podo vender.")
+}
 
-} // nas condiçoes vc pode usar o else if ou o else e dentro do else fazer uma outra condição
-
+console.log("Embarque: \n \n")
+if (idadeDoComprador > 18 && temPassagemComprada){
+    console.log("Boa viagem!")
+} else {
+    console.log("Você não pode embarcar.")
+}
 console.log(listaDeDestinos);
